6c6de74f4cfd4fb2b134fa051450c70e8e1f31b0
Mingzhu <mznie@hotmail.com>
2021-01-09 00:27:12 -0600

    add output_category

--- src/encoded/static/components/bioexperiment.js
+++ src/encoded/static/components/bioexperiment.js
@@ -11,7 +11,8 @@ import { auditDecor } from './audit';
 import { DocumentsPanelReq } from './doc';
 import { DbxrefList } from './dbxref';
 import { FetchedItems } from './fetched';
-import { FileGallery } from './filegallery';
+// import { FileGallery } from './filegallery';
+import { FileGallery1 } from './filegallery1';
 import { CartToggle } from './cart';
 import { ProjectBadge } from './image';
 import { singleTreatment, DisplayAsJson, InternalTags } from './objectutils';
@@ -536,7 +537,8 @@ class Bioexperiment extends React.Component {
                 </Panel>
                 {<BioreplicateTable data={context.bioreplicate} tableTitle="Bioreplicates summary"></BioreplicateTable>}
                 {/* Display the file widget with the facet, graph, and tables */}
-                <FileGallery context={context} encodevers={encodevers} anisogenic={anisogenic} />
+                {/* <FileGallery context={context} encodevers={encodevers} anisogenic={anisogenic} /> */}
+                <FileGallery1 context={context} encodevers={encodevers} anisogenic={anisogenic} />
 
                 <FetchedItems {...this.props} url={experimentsUrl} Component={ControllingExperiments} />
                 {combinedDocuments.length ?
